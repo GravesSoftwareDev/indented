e9df7b3909efe5740a065e14730087d93f88f6d4
Shanna <sg0989463@otc.edu>
2026-06-24 10:42:12 -0500

    maybe another fix?

--- core/settings.py
+++ core/settings.py
@@ -126,9 +126,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
-    },
-}
